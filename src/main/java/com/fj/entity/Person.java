e92f1e07caa781dbde101a992086af2d6869d90e
sweord <sweord@gmail.com>
2017-12-06 10:21:52 +0800

    introduce lombok

--- src/main/java/com/fj/entity/Person.java
+++ src/main/java/com/fj/entity/Person.java
@@ -1,25 +1,22 @@
 package com.fj.entity;
 
-import javax.persistence.Column;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
 
+@Data
 @Entity
 public class Person {
 
 	@Id
 	@GeneratedValue
-	private long id;
+	private Long id;
 
-	@Column
 	private Date gmtCreate;
-
-	@Column
 	private Date gmtModify;
-
-	@Column
 	private String name;
 
 	public Person() {
@@ -32,37 +29,5 @@ public class Person {
 		this.name = name;
 		gmtCreate = new Date();
 		gmtModify = new Date();
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public Date getGmtCreate() {
-		return gmtCreate;
-	}
-
-	public void setGmtCreate(Date gmtCreate) {
-		this.gmtCreate = gmtCreate;
-	}
-
-	public Date getGmtModify() {
-		return gmtModify;
-	}
-
-	public void setGmtModify(Date gmtModify) {
-		this.gmtModify = gmtModify;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
